181133682397d312e8266b7b92eeaa1bc99b5407
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2017-01-02 15:59:04 +0200

    Fix environment loading on Linux machine

--- app/backend/SpotifyWebAuthentication.js
+++ app/backend/SpotifyWebAuthentication.js
@@ -1,35 +1,36 @@
 import electronOauth2 from 'electron-oauth2';
 import SpotifyWebApi from 'spotify-web-api-node';
 
-const config = {
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  authorizationUrl: 'https://accounts.spotify.com/authorize',
-  tokenUrl: 'https://accounts.spotify.com/api/token',
-  useBasicAuthorizationHeader: false,
-  redirectUri: 'http://localhost'
-};
-const windowParams = {
-  alwaysOnTop: true,
-  autoHideMenuBar: true,
-  webPreferences: {
-    nodeIntegration: false
-  }
-};
-const options = {
-  scope: 'user-library-read user-library-modify',
-};
-
-const spotifyOauth = electronOauth2(config, windowParams);
-const spotifyWebApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'http://localhost'
-});
-
 export class SpotifyWebAuthentication {
 
-  authorize = () => spotifyOauth
-      .getAuthorizationCode(options)
-      .then(code => spotifyWebApi.authorizationCodeGrant(code));
+  constructor() {
+    const config = {
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      authorizationUrl: 'https://accounts.spotify.com/authorize',
+      tokenUrl: 'https://accounts.spotify.com/api/token',
+      useBasicAuthorizationHeader: false,
+      redirectUri: 'http://localhost'
+    };
+    const windowParams = {
+      alwaysOnTop: true,
+      autoHideMenuBar: true,
+      webPreferences: {
+        nodeIntegration: false
+      }
+    };
+
+    this.spotifyOauth = electronOauth2(config, windowParams);
+    this.spotifyWebApi = new SpotifyWebApi({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      redirectUri: 'http://localhost'
+    });
+  }
+
+  authorize = () => this.spotifyOauth
+    .getAuthorizationCode({
+      scope: 'user-library-read user-library-modify',
+    })
+    .then(code => this.spotifyWebApi.authorizationCodeGrant(code));
 }
